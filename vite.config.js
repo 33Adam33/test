3c10336f878e4ec84b83847d172b8eb16da2eee1
33Adam33 <86076068+33Adam33@users.noreply.github.com>
2025-07-13 17:13:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/test/', // ⬅️ Replace this with your actual repo name
   plugins: [glsl()],
 });
